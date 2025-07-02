889ede78f56e6fee75557b3556a3246afad9171f
GlowingPiranha <ikimus01@gmail.com>
2025-07-02 18:34:11 +0200

    [apt] fizz

--- script.js
+++ script.js
@@ -11,7 +11,9 @@ for (let i = 1; i <= 100; i++) {
   else if (i % 5 === 0) {
     console.log(`Buzz`)
   }
-
+  else if (i % 3 === 0) {
+    console.log(`Fizz`)
+  }
   else {
     console.log(i)
   }
